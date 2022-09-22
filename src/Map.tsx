46c438721bb4aa9285a6167f1db8a7a4ff7fe7dd
Marin Aeschbach <marin.aeschbach@gmail.com>
2022-09-22 15:51:09 +0200

    FB-13: Add map format settings

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,14 +1,30 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import style from './style.module.css';
 import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { Settings } from './GoogleMapsBlock';
 
 type Props = {
-    apiKey: string;
+    settings: Settings;
 };
 
-export const Map: FC<Props> = ({ apiKey }) => {
+const mapClassNames: Record<string, Record<string, string>> = {
+    '16to9': {
+        inner: style.mapContainerInner16to9,
+        outer: style.mapContainerOuter16to9,
+    },
+    '4to3': {
+        inner: style.mapContainerInner4to3,
+        outer: style.mapContainerOuter4to3,
+    },
+    '1to1': {
+        inner: style.mapContainerInner1to1,
+        outer: style.mapContainerOuter1to1,
+    },
+};
+
+export const Map: FC<Props> = ({ settings }) => {
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: apiKey,
+        googleMapsApiKey: settings.apiKey,
     });
 
     if (!isLoaded) {
@@ -16,8 +32,23 @@ export const Map: FC<Props> = ({ apiKey }) => {
     }
 
     return (
-        <div className={style.container}>
-            <GoogleMap zoom={10} center={{ lat: 44, lng: -80 }} mapContainerClassName={style.mapContainer}></GoogleMap>
+        <div
+            className={
+                !settings.customMapFormat
+                    ? [style.mapContainerOuter, mapClassNames[settings.formatPreset].outer].join(' ')
+                    : undefined
+            }
+            style={settings.customMapFormat ? { height: parseInt(settings.fixedHeight) } : undefined}
+        >
+            <GoogleMap
+                zoom={10}
+                center={{ lat: 44, lng: -80 }}
+                mapContainerClassName={
+                    !settings.customMapFormat
+                        ? [style.mapContainerInner, mapClassNames[settings.formatPreset].inner].join(' ')
+                        : style.mapContainerCustom
+                }
+            ></GoogleMap>
         </div>
     );
 };
