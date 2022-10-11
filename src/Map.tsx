58f60c20c4dc4fad106fb04c2736c99943995ba1
Juerg Hunziker <juerg.hunziker@liip.ch>
2022-10-11 10:30:51 +0200

    fix removal changing of markers and map state

--- src/Map.tsx
+++ src/Map.tsx
@@ -66,7 +66,7 @@ export const Map: FC<Props> = ({ setMarkers, setMapState, isEditing, settings })
                     }
                 }
             }, 500),
-        [isEditing, customMapFormat, formatPreset, fixedHeight]
+        [isEditing, customMapFormat, formatPreset, fixedHeight, markers]
     );
 
     const fitBounds = () => {
