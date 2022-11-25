583f92922e3c299f332dd6248bf15dbfd754c07f
Juerg Hunziker <juerg.hunziker@liip.ch>
2022-11-25 08:27:49 +0100

    remove unused env variable

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_GOOGLE_MAP_API_KEY: string;
+    readonly VITE_EXAMPLE_ENV: string;
     // more env variables...
 }
 
