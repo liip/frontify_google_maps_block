4408cb7a55723c8275f1cc51787cfb78f86ad116
Juerg Hunziker <juerg.hunziker@liip.ch>
2022-12-13 14:17:35 +0100

    fix codestyle

--- src/Map.tsx
+++ src/Map.tsx
@@ -240,7 +240,8 @@ export const Map: FC<Props> = ({ isEditing, settings, setSettings, setIsReadyFor
                             Reset Zoom
                         </Button>
                         <Text as="p" size="small">
-                            The current map position and zoom level are persisted as soon as you switch back to the view mode.
+                            The current map position and zoom level are persisted as soon as you switch back to the view
+                            mode.
                         </Text>
                     </Stack>
                     {Object.keys(state.markers).map((markerId) => (
