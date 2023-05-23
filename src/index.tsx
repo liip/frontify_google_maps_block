550b7345d783a27700528c7959e750520ea18439
ragi96 <rafael.giezendanner@frontify.com>
2023-05-23 10:31:29 +0200

    remove double

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { GoogleMapsBlock } from './GoogleMapsBlock';
-import { settings } from './settings';
-import { defineBlock } from '@frontify/guideline-blocks-settings';
-
-export default defineBlock({
-    block: GoogleMapsBlock,
-    settings,
-});
